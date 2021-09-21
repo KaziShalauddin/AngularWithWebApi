321e0fdb0ec22089f47d29cda4c13c11698e39e7
KaziShalauddin <shalauddin99@gmail.com>
2021-09-21 07:34:13 +0600

    modification to force json response

--- WebApi/WebApi/App_Start/WebApiConfig.cs
+++ WebApi/WebApi/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Net.Http.Headers ;
 
 namespace WebApi
 {
@@ -19,6 +20,8 @@ namespace WebApi
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
