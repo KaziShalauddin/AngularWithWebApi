5860d19aad2ca5a2acba7bea8ad79ab3f26266c2
KaziShalauddin <shalauddin99@gmail.com>
2021-09-24 19:09:19 +0600

    enable cors

--- WebApi/WebApi/App_Start/WebApiConfig.cs
+++ WebApi/WebApi/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using System.Net.Http.Headers ;
+using System.Web.Http.Cors;
 
 namespace WebApi
 {
@@ -22,6 +23,7 @@ namespace WebApi
             );
 
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
         }
     }
 }
