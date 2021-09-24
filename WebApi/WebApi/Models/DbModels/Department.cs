4bd2775e9f1add502a53afc97e1b7c0e51d96ee2
KaziShalauddin <shalauddin99@gmail.com>
2021-09-24 19:24:47 +0600

    added db_models

---
+++ WebApi/WebApi/Models/DbModels/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models.DbModels
+{
+    [Table("Departments")]
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+
+        public ICollection<Employee> Employees { get; set; }
+    }
+}
---
+++ WebApi/WebApi/Models/DbModels/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models.DbModels
+{
+    [Table("Employees")]
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public DateTime JoiningDate { get; set; }
+        public string PhotoPath { get; set; }
+
+        public int DepartmentId { get; set; }
+        [ForeignKey("DepartmentId")]
+        public Department Department { get; set; }
+    }
+}
